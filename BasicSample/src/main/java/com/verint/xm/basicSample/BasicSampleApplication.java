801d09c4f1ddadc791bc2723a70c2ee704b1e609
Wayne Burkett <wayne.burkett@verint.com>
2025-02-11 19:40:37 -0500

    VOC-53033: bring the basic samples inline with each other and have them use the mobsdk-basicsample config

--- BasicSample/src/main/java/com/verint/xm/basicSample/BasicSampleApplication.java
+++ BasicSample/src/main/java/com/verint/xm/basicSample/BasicSampleApplication.java
@@ -1,15 +1,44 @@
 package com.verint.xm.basicSample;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.verint.xm.sdk.Core;
 
-public class BasicSampleApplication extends Application {
+public class BasicSampleApplication extends Application implements Core.VerintSDKListener {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		// Notify ForeSee SDK of application start
+		// Enable debug logs (not typically used in production, but helpful
+		// when testing)
 		Core.setDebugLogEnabled(true);
-		Core.start(this);
+
+		// Set a VerintSDKListener (used to determine when the SDK has started (or if
+		// it fails to start)
+		Core.setSDKListener(this);
+
+		// Set the configuration container. This defaults to "live" and only
+		// needs to be supplied when testing with other containers (e.g. "draft").
+		Core.setConfigurationContainer("live");
+
+		// Start the SDK with this app's site key
+		Core.startWithSiteKey(this, "mobsdk-basicsample");
+	}
+
+	// Core.VerintSDKListener
+
+	@Override
+	public void onSDKStarted() {
+		Log.i("VerintSDKListener", "onSDKStarted");
+	}
+
+	@Override
+	public void onSDKStarted(Core.VerintError verintError, String errorDescription) {
+		Log.i("VerintSDKListener", "onSDKStarted with error: " + verintError.name() + ": " + errorDescription);
+	}
+
+	@Override
+	public void onSDKFailedToStart(Core.VerintError verintError, String errorDescription) {
+		Log.i("VerintSDKListener", "onSDKFailedToStart with error: " + verintError.name() + ": " + errorDescription);
 	}
 }
