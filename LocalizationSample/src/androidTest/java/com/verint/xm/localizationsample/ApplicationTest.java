99af4285f1bf80d966e00165779f13ae7b7d6903
Shang Wu <shang.wu@verint.com>
2026-03-16 13:24:34 -0700

    VOC-63459:update unit test libraries.

--- LocalizationSample/src/androidTest/java/com/verint/xm/localizationsample/ApplicationTest.java
+++ LocalizationSample/src/androidTest/java/com/verint/xm/localizationsample/ApplicationTest.java
@@ -1,13 +1,25 @@
 package com.verint.xm.localizationsample;
 
-import android.app.Application;
+import android.content.Context;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super();
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.verint.xm.localizationsample", appContext.getPackageName());
     }
 }
+
